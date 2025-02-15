4a0420b28d29842ce19ece01c21ecbb86a5305b3
Anders Skarby <anders@skarby.info>
2025-02-15 08:49:34 +0100

    Added configuration of ASDF and added more shell aliases

---
+++ zsh/.aliases/git.zsh
@@ -0,0 +1,17 @@
+#####################
+# Basic git aliases #
+#####################
+alias gap="git add -p"
+alias gcls="git restore ."
+
+################################################
+# Combinatory tooling for advanced git aliases #
+################################################
+
+# Switches to another local branch (selection through FZF)
+alias gsw='git branch | grep -v "^\*" | fzf --height=20% --reverse --info=inline | xargs git checkout'
+
+##################################
+# Other tooling, but git related #
+##################################
+alias lg)"lazygit"
