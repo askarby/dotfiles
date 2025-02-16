531d83b4b4614bcf15659ac04ff373d167c824e8
Anders Skarby <anders@skarby.info>
2025-02-16 09:21:38 +0100

    Added more aliases

--- zsh/.aliases/git.zsh
+++ zsh/.aliases/git.zsh
@@ -3,6 +3,9 @@
 #####################
 alias gap="git add -p"
 alias gcls="git restore ."
+alias gpush="git push origin HEAD"
+alias gf="git fetch"
+alias gam="git add -A && git ci"
 
 ################################################
 # Combinatory tooling for advanced git aliases #
