2a84787445d7429e177366c97606e45e789ebf31
Anders Skarby <anders@skarby.info>
2025-05-14 17:03:30 +0200

    Tweaks to noice configuration, don't show popup for commands or search

--- neovim/.config/nvim/lua/plugins/noice.lua
+++ neovim/.config/nvim/lua/plugins/noice.lua
@@ -1,27 +1,30 @@
 return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-        -- add any options here
-    },
-    on_attach = function()
-        vim.keymap.set({"n", "i", "s"}, "<c-f>", function()
-            if not require("noice.lsp").scroll(4) then
-                return "<c-f>"
-            end
-        end, {
-            silent = true,
-            expr = true
-        })
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		-- add any options here
+		cmdline = {
+			view = "cmdline", -- moves command line to bottom
+		},
+	},
+	on_attach = function()
+		vim.keymap.set({ "n", "i", "s" }, "<c-f>", function()
+			if not require("noice.lsp").scroll(4) then
+				return "<c-f>"
+			end
+		end, {
+			silent = true,
+			expr = true,
+		})
 
-        vim.keymap.set({"n", "i", "s"}, "<c-b>", function()
-            if not require("noice.lsp").scroll(-4) then
-                return "<c-b>"
-            end
-        end, {
-            silent = true,
-            expr = true
-        })
-    end,
-    dependencies = {"MunifTanjim/nui.nvim", "rcarriga/nvim-notify"}
+		vim.keymap.set({ "n", "i", "s" }, "<c-b>", function()
+			if not require("noice.lsp").scroll(-4) then
+				return "<c-b>"
+			end
+		end, {
+			silent = true,
+			expr = true,
+		})
+	end,
+	dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
 }
