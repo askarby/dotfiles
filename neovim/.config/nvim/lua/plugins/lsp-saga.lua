163337bed7f029dd06f341c5c2b306a53151b5ae
Anders Skarby <anders@skarby.info>
2025-05-30 09:47:29 +0200

    Added additional keybindings to Lspsaga

--- neovim/.config/nvim/lua/plugins/lsp-saga.lua
+++ neovim/.config/nvim/lua/plugins/lsp-saga.lua
@@ -4,7 +4,20 @@ return {
 		config = function()
 			require("lspsaga").setup({})
 
-			vim.keymap.set("n", "K", "<CMD>Lspsaga hover_doc<CR>")
+			vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>")
+			vim.keymap.set(
+				"n",
+				"<leader>ci",
+				"<cmd>Lspsaga incoming_calls<CR>",
+				{ desc = "[C]ode Show [I]ncoming calls" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>co",
+				"<cmd>Lspsaga outgoing_calls<CR>",
+				{ desc = "[C]ode Show [O]utgoing calls" }
+			)
+			vim.keymap.set("n", "<leader>to", "<cmd>Lspsaga outline<CR>", { desc = "[T]oggle [O]utline" })
 		end,
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter", -- optional
